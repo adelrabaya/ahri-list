aa38993b3efd33bab4effdb5e967828c98f58f32
adelrabaya <adel.rabaya@gmail.com>
2022-09-15 16:14:45 +0300

    initial

---
+++ pages/about.js
@@ -0,0 +1,19 @@
+import Head from 'next/head'
+
+const About = () => {
+    return ( 
+        <>
+            <Head>
+                <title>Ahri List | About</title>
+                <meta name="keywords" content="ninjas" />
+            </Head>
+            <div>
+            <h1>About</h1>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Architecto unde quisquam necessitatibus quas ut aut molestiae. Maxime laborum, natus eos laudantium, mollitia sequi iusto, soluta cumque reprehenderit ipsa asperiores eum necessitatibus, voluptatibus sunt fugit tenetur accusantium et officiis sit eos alias.</p>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Architecto unde quisquam necessitatibus quas ut aut molestiae. Maxime laborum, natus eos laudantium, mollitia sequi iusto, soluta cumque reprehenderit ipsa asperiores eum necessitatibus, voluptatibus sunt fugit tenetur accusantium et officiis sit eos alias.</p>
+            </div>
+        </>
+        );
+}
+ 
+export default About;
